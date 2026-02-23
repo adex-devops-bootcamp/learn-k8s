768358455ec48be8f42814b627ead46ca29e590d
Yaman Man Shakya <103587873+yshakya@users.noreply.github.com>
2026-02-23 14:28:15 +0000

    draft: use values from yaml

--- lab 4/install.sh
+++ lab 4/install.sh
@@ -1,6 +1,3 @@
 helm install cluster-autoscaler autoscaler/cluster-autoscaler \
   -n kube-system \
-  --set autoDiscovery.clusterName=<CLUSTER_NAME> \
-  --set awsRegion=<AWS_REGION> \
-  --set rbac.serviceAccount.create=false \
-  --set rbac.serviceAccount.name=cluster-autoscaler
+  -f values.yaml
